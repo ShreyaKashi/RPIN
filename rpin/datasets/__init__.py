8ba4c85f405bbf495fcd6f7b4bbbac5774eeba20
Mingqi Jiang <mingqijiang@MacBook-Pro.local>
2023-08-15 23:17:22 -0700

    origin point cloud dataloader added

--- rpin/datasets/__init__.py
+++ rpin/datasets/__init__.py
@@ -4,3 +4,5 @@ from rpin.datasets.simb import SimB
 from rpin.datasets.phyre import PHYRE
 from rpin.datasets.phyreo import PHYREO
 from rpin.datasets.vid import VidPHYRECls
+from rpin.datasets.phys_pc import Phys_pc
+from rpin.datasets.ss_pc import SS_PC
